5679cb1d92b673a1f65f28ea9e59f76f9c03867a
321alwaysonline <57414628+321alwaysonline@users.noreply.github.com>
2019-11-07 19:22:32 +0100

    Update Demo_NMF.m

--- Demos/Demo_NMF.m
+++ Demos/Demo_NMF.m
@@ -1,176 +1,170 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% BELLA.m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%% ONMF_driver.m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% function [ x,f,out ] = BELLA(x0,func,kernel,subprob,options) 
-% BELLA is a locally superlinearly convergent Bregman forward-backward  
-% splitting method for solving nonsmooth nonconvex composite optimization
-% 
-%                    min f(x) + g(x) 
-%      where x=(x_1,...,x_N) and
-%            f is relatively smooth;
-%            g is proper and lsc. 
-%
-% INPUT:
-%
-% func                 % function handle for the objective function
-% kernel               % kernel of Bregman distance
-% subprob              % function handle for associated subproblems
-% x0                   % initial point
-% options              % structure including the parameteres of scheme
-%
-%   .gamma0            % step-sizes for proximal operators 
-%   .MaxNumIter        % maximum number of iterations
-%   .MaxNumFunEval     % maximum number of function evaluations
-%   .MaxNumGradEval    % maximum number of gradient evaluations
-%   .TimeLimit         % maximum running time
-%   .Stopping_Crit     % stopping criterion
-%
-%                      % 1 : stop if MaxNumIter is reached (default)
-%                      % 2 : stop if MaxNumFunEval is reached
-%                      % 3 : stop if MaxNumGradEval is reached
-%                      % 4 : stop if TimeLimit is reached
-%
-% OUTPUT:
-%
-% x                    % the best approximation of the optimizer
-% f                    % the best approximation of the optimum
-% out                  % structure including more output information
-%
-%   .T                 % running time
-%   .Niter             % total number of iterations
-%   .Nfunc             % total number of function evaluations
-%   .Ngrad             % total number of gradient evaluations
-%   .F                 % array including all function values             
-%   .Status            % reason of termination
-
-% REFERENCE: 
-%
-% [1] Bregman forward-backward splitting for nonconvex composite optimization:
-% superlinear convergence to nonisolated critical points, 
-% Submitted,(2019)
-%           
-% WRITTEN BY: 
-%
-% Masoud Ahookhosh
-% Department of Electrical Engineering(ESAT-STADIUS), KU Leuven, Belgium
-%
-% Yang Lu
-% Department of Mathematics (Section of Statistics), KU Leuven, Belgium
-%
-% LAST UPDATE: 
-%
-% November 2019
-%
+format compact 
+clear
+clf
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+Slist = {'BELLA'};
+flag  = 'dense';
+%flag  = 'sparse';
 
-function [ x,f,out ] = BELLA( x0,func,kernel,subprob,options )
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%% Initializing and setting the parameters %%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-format long;
-
-% ================ Error messages for input and output =================
-if nargin > 5
-    error('The number of input arguments is more than what is needed');
-elseif nargin < 5
-    error('The number of input arguments is not enough');
-end
-
-if isempty(func)
-    error('BPALM needs the function handle func to be defined');
-elseif ~isa(func,'function_handle')
-    error('func should be a function handle');
-end
-
-if isempty(subprob)
-    error('BPALM needs the function handle subprob to be defined');
-elseif ~isa(subprob,'function_handle')
-    error('subprob should be a function handle');
-end
-
-% =================== initializing the parameters ======================
-% ===== user has requested viewing the default values of "options" =====
-[MaxNumIter,MaxNumFunEval,MaxNumGradEval,TimeLimit, ...
-    flag_time,Stopping_Crit] = Initialization(options);
-
-if flag_time == 1
-    Time(1) = 0;
-end
-
-N        = length(x0);
-gammak   = options.gamma0;
-xk       = x0;
-Niter    = 1;
-fxk      = func(xk,0);
-F        = fxk;
-Nfunc    = 1;
-Ngrad    = 0;
-StopFlag = 0;
-
-outx=[]; % Hien added
+lambda        = 10;
+Ttol          = 15;
+maxit         = 10000;
+Stopping_Crit = 4;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%% Main body of BELLA.m %%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-T0 = tic;
-
-% ======================= start of the main loop =======================
-while ~StopFlag
-    xk1 = xk;
-        % solving subproblem %
-        optsub.block   = i;
-        gammaki        = gammak(i);
-        optsub.gammaki = gammaki;
-        [fxk1,gfki]    = func(xk1,i);
-        Nfunc          = Nfunc+1;
-        Ngrad          = Ngrad+1;
-        [hxk1,ghki]    = kernel(xk1,i);
-        optsub.gfki    = gfki;
-        optsub.ghki    = ghki;
-        xk1i           = subprob(xk1,optsub); 
-        % updating the sequence %
-        xk1(i)         = {xk1i};
-        Niter          = Niter+1
-        F              = [F,fxk1];
-    xk = xk1;
-    outx=[outx; xk1]; % Hien added
+for i=1:1
+    % ==================================================================
+    % Dimensions of input matrix and rank
+    m = 200;
+    n = 2000;
+    r = 10;
+    
+    if strcmp(flag,'dense') 
+        % Generation of synthetic data set
+        U = rand(m,r);
+        V = zeros(r,n);
+        for k = 1 : n
+            V(randi(r),k) = rand(1);
+        end
+        for j = 1 : r
+            V(j,:) = V(j,:)/norm(V(j,:));
+        end
+        X = U*V; % no noise
+        R = rand(m,n);
+        X = X + 0.05*R/norm(R,'fro')*norm(X,'fro');
+   
+    elseif strcmp(flag,'sparse') 
+        % Generation of synthetic data set
+        V = sprand(r,n,0.5);
+        U = sprand(m,r,0.5);
+        X = U*V; % no noise
+        R = sprand(m,n,0.5);
+        X = X+0.05*R; 
+    end
+    
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%% Start of impelementations %%%%%%%%%%%%%%%%%%%%%
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
+    % Initialization
+    [U0,V0] = NNDSVD(X',r,0); % SVD-based initialization
+    x0(1)   = {V0'};
+    x0(2)   = {U0'};
+    
+    nu     = 2;
+    beta1  = 1;
+    alpha2 = 1;
+    beta2  = 1;
+    L0(1)  = 2/(beta1*beta2);
+    gamma1 = 1/L0(1)-eps; 
+    max3t  = max([lambda/alpha2; 2*lambda/(beta1*beta2);lambda/beta2]);
+    L0(2)  = 6*max3t;
+    gamma2 = 1/L0(2)-eps;
+    
+    opt.X      = X;
+    opt.lambda = lambda;
+    func       = @ (varargin) ONMF(opt,varargin{:});
+    
+    opth.beta1  = beta1;
+    opth.alpha2 = alpha2;
+    opth.beta2  = beta2;
+    kernel      = @ (varargin) hNMF(opth,varargin{:});
+    
+    subprob = @ (varargin) subONMF(beta1,alpha2,beta2,varargin{:});
+    
+    options.MaxNumIter     = maxit;
+    options.MaxNumFunEval  = 1000;
+    options.MaxNumGradEval = 5000;
+    options.Stopping_Crit  = Stopping_Crit;
+    options.TimeLimit      = Ttol;
+    options.epsilon        = 1e-5;
+    options.gamma0         = [gamma1,gamma2];
+    options.lambda         = lambda;
+    options.L0             = L0;
+    options.nu             = nu;
   
-    % ================== checking stopping criteria ====================
-    Time = toc(T0); 
-    [StopFlag, Status] = StopCriterion(Niter,Nfunc,Ngrad, ...
-    Time,MaxNumIter,MaxNumFunEval,MaxNumGradEval,TimeLimit, ...
-    Stopping_Crit);   
+    % ======================== applying solvers ========================
+    for j = 1:length(Slist) 
+
+        switch Slist{j}
+                           
+            case 'BPALM'
+                fprintf('Running BPALM ...\n')
+                tic
+                [x,f,out] = BPALM( x0,func,kernel,subprob,options );         
+                toc
+                F           = out.F;
+                F_best      = F(1);
+                fun_BPALM   = F(end);
+                t_BPALM     = out.T;
+                nfunc_BPALM = out.Nfunc;
+                f_eval{j}   = F';
+                   
+            case 'A-BPALM1'
+                fprintf('Running A-BPALM1 ...\n')
+                Lm             = (1e-2)*L0;
+                options.L0     = Lm;
+                options.gamma0 = 1./Lm-eps;
+                tic
+                [ x,f,out ] = A_BPALM1( x0,func,kernel,subprob,options );         
+                toc
+                F             = out.F;
+                F_best        = F(1);
+                fun_abpalm1   = F(end);
+                t_abpalm1     = out.T;
+                nfunc_abpalm1 = out.Nfunc;
+                f_eval{j}     = F';
+            
+            case 'A-BPALM2'
+                fprintf('Running A-BPALM2 ...\n')
+                Lm             = (1e-1)*L0;
+                options.L0     = Lm;
+                options.gamma0 = 1./Lm-eps;
+                tic
+                [ x,f,out ] = A_BPALM2( x0,func,kernel,subprob,options );         
+                toc
+                F             = out.F;
+                F_best        = F(1);
+                fun_abpalm2   = F(end);
+                t_abpalm2     = out.T;
+                nfunc_abpalm2 = out.Nfunc;
+                f_eval{j}     = F';
+
+        end
+    end
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Outputs %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Status
-x          = xk;
-f          = func(x,0);
-T          = Time;
-out.T      = T;
-out.F      = F';
-out.Niter  = Niter;
-out.Nfunc  = Nfunc;
-out.Ngrad  = Ngrad;
-out.Status = Status;
-
-out.x=outx; % Hien added
-
-if flag_time == 1
-    out.Time = Time;
+figure(1)
+for k = 1: length(Slist)
+    switch Slist{k}                
+        case 'BPALM'
+            xk = floor(exp((1:20)*(log(length(f_eval{k}))/20)));
+            Fk = f_eval{k}(xk);
+            semilogy(xk,Fk,'Color',[0.53,0.33,0.65], ...
+                        'Marker','v','LineWidth',2)
+        case 'A-BPALM1'
+            xk = floor(exp((1:15)*(log(length(f_eval{k}))/15)));
+            Fk = f_eval{k}(xk);
+            semilogy(xk,Fk,'Color',[0.83,0.36,0], ...
+                     'Marker','d','LineWidth',2) 
+        case 'A-BPALM2'
+            xk = floor(exp((1:10)*(log(length(f_eval{k}))/10)));
+            Fk = f_eval{k}(xk);
+            semilogy(xk,Fk,'Color',[0.26,0.63,0.79],...
+                        'Marker','p','LineWidth',2) 
+    end  
+    hold on
+    xlabel('iterations','FontSize',20);
+    ylabel('function values','FontSize',20);
+    legend('BELLA','FontSize',14);
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%% End of BELLA.m %%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%% End of the script %%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
